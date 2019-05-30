1d3a366058e34721fe385e990e19f84ebf7d04a9
wangmin <0201.mj.lee@gmail.com>
2019-05-30 16:29:38 +0900

    feat : require readline

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 const net = require('net');
+const ReadLine = require('net');
 const UserManager = require('./user_manager.js');
+
+const readLine = ReadLine.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
 const socket = net.connect(
     { host: 'localhost', port: 3000 }
@@ -11,7 +17,6 @@ const socket = net.connect(
 socket.on('data', (data) => {
     console.log(data.toString());
 });
-
 
 
 
