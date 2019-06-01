f86d904c9503192b848fb458728e228fc592a4d7
wangmin <0201.mj.lee@gmail.com>
2019-06-01 19:56:34 +0900

    feat : fix getUserInfo function
    - fix getUserInfo function
    - add doAction function
    - fix choice Action function

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const net = require('net');
-const ReadLine = require('net');
+const ReadLine = require('readline');
 const UserManager = require('./user_manager.js');
+
+const userManager = new UserManager;
 
 const readLine = ReadLine.createInterface({
     input: process.stdin,
@@ -11,6 +13,7 @@ const socket = net.connect(
     { host: 'localhost', port: 3000 }
     , () => {
         console.log(`Welcome to Wang's Blackjack`);
+        main();
     }
 );
 
@@ -18,33 +21,45 @@ socket.on('data', (data) => {
     console.log(data.toString());
 });
 
+const choiceAction = () => {
+    return new Promise((resolve, reject) => {
+        readLine.question(`input '1' or '2'\n< 1. log in >   < 2. sign up > `, (input) => {
+            if(input){
+                if(input !== '1' && input !== '2'){
+                    console.log(`다시입력하세요.`);
+                    choiceAction().then(doAction).catch(err => {
+                        console.log('Error',err.message);
+                    });
+                }
+                resolve(input);
+            }
+            reject(new Error("request is failed"));
+        })
+    });
+};
 
-const choiceAction = () =>{
-    console.log(`       input '1' or '2'\n  < 1. log in >   < 2. sign up >`);
-    readLine.on("line", line => {
-        if(line === '1'){
-            logIn();
-        }if(line === '2'){
-            signUp();
-        }else{
-            console.log(`다시입력하세요`);
-            choiceAction();
-        };
-    }).on("close", () => {
-        process.exit();
+const logIn = ()=>{
+    return userManager.login();
+}
+const signUp = ()=>{
+    return userManager.signUp();
+}
+
+const doAction = (action) =>{
+    console.log('in do action');
+    if(action === '1'){
+        return logIn();
+    }else if(action === '2'){
+        return signUp();
+    };
+}
+
+const getUserInfo = () =>{
+    return choiceAction().then(doAction).catch(err => {
+        console.log('Error',err.message);
     });
 }
 
-const logIn = ()=>{
-    const user = UserManager.login();
-}
-const signUp = ()=>{
-    const user = UserManager.signUp();
-}
-
-
 const main = () =>{
-    choiceAction();
+    const userInfo = getUserInfo();
 }
-
-main();
