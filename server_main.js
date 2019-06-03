b3ce93ee1b29239153d76e135f252826eba8c3d5
wangmin <0201.mj.lee@gmail.com>
2019-06-03 15:46:09 +0900

    feat : feat createUserDataFile function
    - add calling createUserDataFile fucntion

--- server_main.js
+++ server_main.js
@@ -2,14 +2,26 @@ const net = require('net');
 const fs = require('fs');
 
 
+const createUserDataFile = () =>{
+    if(!fs.existsSync(`./data`)){
+        fs.mkdirSync(`./data`);
+    }
+    fs.writeFileSync(`./data/userData.txt`,'','utf8');
+    console.log('userData.txt has been created.');
+}
+
 const server = net.createServer((socket) =>{
     console.log('connected');
     socket.setEncoding('utf8');
     
     socket.on('data', (data) =>{
+        if(!fs.existsSync(`./data/userData.txt`)){
+            createUserDataFile(); 
+        }
         console.log('received data : '+data);
         socket.write('false');
     });
+    
     socket.on('end', function(){
         console.log('disconnected');
     });
