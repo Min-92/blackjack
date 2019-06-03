645edb7040c6d346621c0c997f735821a938d81f
wangmin <0201.mj.lee@gmail.com>
2019-06-03 20:04:13 +0900

    feat : feat shuffleDcek method

--- dealer.js
+++ dealer.js
@@ -2,5 +2,16 @@ module.exports = class Dealer{
     constructor(){
         this.hand;
     }
+    shuffleDeck(deck){
+        const newDeck = Array.from(deck);
+        for(let i = newDeck.length -1; i > 0 ; i--){
+            const randomIndex = Math.floor(Math.random()*(i+1));
+            const randomCard = newDeck[i];
+            newDeck[i] = newDeck[randomIndex];
+            newDeck[randomIndex] = randomCard;
+        }
+        return newDeck;
+    }    
+
 
 }
