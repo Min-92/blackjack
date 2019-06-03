c4df0c44d4a9eab32cfc9b901c8092856f9e95ab
wangmin <0201.mj.lee@gmail.com>
2019-06-03 18:26:47 +0900

    feat : feat signup method
    - fix login method

--- user_manager.js
+++ user_manager.js
@@ -27,19 +27,33 @@ module.exports = class UserManager{
         tempUser.id = this.inputData('Please input your ID> ');
         tempUser.password = this.inputDataHide('Please input your password> ');
         this.socket.write(`login$${tempUser.id}$${tempUser.password}`);
-        const result = await this.getUserData();
-        if(result === 'false'){
+        const result = JSON.parse(await this.getUserData());
+        if(result === false){
             console.log('ID 나 password 가 올바르지 않습니다.');
             await this.login();
         }
         this.socket.removeAllListeners();
-        this.user.id = tempUser.id;
-        this.user.password = tempUser.password;
+        this.user = result;
         return this.user;
     }
 
-    signUp(){
-        console.log('in user manager signup');
-        return 0;
+    async signUp(){
+        const tempUser = {};
+        tempUser.id = this.inputData('Please input your ID> ');
+        tempUser.password = this.inputDataHide('Please input your password> ');
+        tempUser.password2 = this.inputDataHide('Please input your password again> ');
+        if(tempUser.password !== tempUser.password2){
+            console.log('비밀번호가 일치하지 않습니다.');
+            await this.signUp();
+        }
+        this.socket.write(`signUp$${tempUser.id}$${tempUser.password}`);
+        const result = JSON.parse(await this.getUserData());
+        if(result === false){
+            console.log('ID 가 이미 존재합니다.');
+            await this.signUp();
+        }
+        this.socket.removeAllListeners();
+        this.user = result;
+        return this.user;
     }
 };
