3a35508b806719ad280b74d1aa7a3c8e5058175f
wangmin <0201.mj.lee@gmail.com>
2019-06-03 14:48:09 +0900

    feat : feat login method
    - add constructor socket, readlineSync, user object
    - add inputData method
    - add inputDataHide method
    - add getUserData method

--- user_manager.js
+++ user_manager.js
@@ -1,7 +1,45 @@
 module.exports = class UserManager{
-    constructor(){
+    constructor(socket, readlineSync){
+        this.socket = socket;
+        this.readlineSync = readlineSync;
+        this.user = {};
+    }
+
+    inputData(question){
+        return this.readlineSync.question(`${question}`);
+    }
+    
+    inputDataHide(question){
+        return this.readlineSync.question(`${question}`, {hideEchoBack: true});
+    }
+
+    getUserData(){
+        return new Promise(resolve =>{
+            this.socket.on('data', (data) =>{
+                resolve(data.toString());
+            });
+        })
 
     }
 
+    async login() {
+        const tempUser = {};
+        tempUser.id = this.inputData('Please input your ID> ');
+        tempUser.password = this.inputDataHide('Please input your password> ');
+        this.socket.write(`login$${tempUser.id}$${tempUser.password}`);
+        const result = await this.getUserData();
+        if(result === 'false'){
+            console.log('ID 나 password 가 올바르지 않습니다.');
+            await this.login();
+        }
+        this.socket.removeAllListeners();
+        this.user.id = tempUser.id;
+        this.user.password = tempUser.password;
+        return this.user;
+    }
 
+    signUp(){
+        console.log('in user manager signup');
+        return 0;
+    }
 };
