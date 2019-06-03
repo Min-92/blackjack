22056f7c2075d7e27d71af376354aa47438ec7ba
wangmin <0201.mj.lee@gmail.com>
2019-06-03 19:39:57 +0900

    feat : require deck.js
    - add constructor

--- blackjack.js
+++ blackjack.js
@@ -1,3 +1,25 @@
+const getNewDeck = require('./deck.js');
+
 module.exports = class Blackjack{
-    
+    constructor(player,socket,readlineSync){
+        this.socket = socket;
+        this.readlineSync = readlineSync;
+        
+        this.player = player;
+        this.dealer;
+        this.deck;
+    }
+
+    startGame(){
+        this.deck = getNewDeck();
+
+    }
+
+
+
+
+
+
+
+
 }
