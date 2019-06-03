588c8ba288cf2f28769c17700d8e39079d2b2a9f
wangmin <0201.mj.lee@gmail.com>
2019-06-03 17:11:58 +0900

    feat : feat serverMain class
    - add updateUserData method
    - add login method

--- server_main.js
+++ server_main.js
@@ -1,38 +1,65 @@
 const net = require('net');
 const fs = require('fs');
 
-const createUserDataFile = () =>{
-    if(!fs.existsSync(`./data`)){
-        fs.mkdirSync(`./data`);
+class ServerMain{
+    constructor(){
+        this.userData;
     }
-    fs.writeFileSync(`./data/userData.txt`,'','utf8');
-    console.log('userData.txt has been created.');
+    createUserDataFile(){
+        if(!fs.existsSync(`./data`)){
+            fs.mkdirSync(`./data`);
+        }
+        fs.writeFileSync(`./data/userData.txt`,'','utf8');
+        console.log('userData.txt has been created.');
+    };
+
+    parseData(data){
+        const dataArray = data.split('$');
+        const command = {};
+        switch(dataArray[0]){
+            case 'login' :
+                command.action = 'login';
+                command.id = dataArray[1];
+                command.pw = dataArray[2];
+        }
+        return command;
+    };
+
+    updateUserData(){
+        this.userdata = JSON.parse(fs.readFileSync('./data/userData.txt').toString());
+    }
+
+    login(command){
+        this.updateUserData();
+        const obj = {};
+        return obj;
+    }
+    
+
 }
 
-const parseData = (data) => {
-    const dataArray = data.split('$');
-    const command = {};
-    switch(dataArray[0]){
-        case 'login' :
-            command.action = 'login';
-            command.id = dataArray[1];
-            command.pw = dataArray[2];
-    }
-    return command;
-}
+
 
 const server = net.createServer((socket) =>{
     console.log('connected');
+    const main = new ServerMain();
+
     socket.setEncoding('utf8');
     
     socket.on('data', (data) =>{
         if(!fs.existsSync(`./data/userData.txt`)){
-            createUserDataFile(); 
+            main.createUserDataFile(); 
         }
         console.log('received data : '+data);
-        const command = JSON.stringify(parseData(data));
+        const command = main.parseData(data);
+        const action = command.action;
+        const sendingData = main[`${action}`](command);
+        // global[`${action}`]();
+        // this[`${action}Check`](...condition);
 
-        socket.write(`${command}`);
+        
+        // const command = JSON.stringify(parseData(data));
+        socket.write(`${JSON.stringify(sendingData)}`);
     });
 
     socket.on('end', function(){
