af6d246c235fd673e72e251eca53c1272df1e5a1
wangmin <0201.mj.lee@gmail.com>
2019-06-04 14:17:46 +0900

    feat : feat initObject method
    - require player.js

--- blackjack.js
+++ blackjack.js
@@ -1,42 +1,53 @@
 const getNewDeck = require('./deck.js');
 const Dealer = require('./dealer.js');
+const Player = require('./player.js');
 
 module.exports = class Blackjack{
-    constructor(player,socket,readlineSync){
+    constructor(user,socket,readlineSync){
         this.socket = socket;
         this.readlineSync = readlineSync;
+        this.userInfo = userInfo;
         
-        this.player = player;
+        this.player;
         this.dealer;
         this.deck;
         this.bettingMoney;
     }
 
-    shuffleDeck(){
+    shuffleDeck(deck){
         console.log('shuffling cards...');
-        this.deck = this.dealer.shuffleDeck(this.deck);
+        return this.dealer.shuffleDeck(deck);
     }
     printPlayerMoney(){
         console.log(`Your money : ${this.player.money}`);
     }
     
-    betMoney(){
-        this.bettingMoney = Number(this.readlineSync.question(`베팅할 금액을 입력하세요. 소지금 : ${this.player.money}> `));
-        this.player.money -= this.bettingMoney;
+    betMoney(player){
+        this.bettingMoney = Number(this.readlineSync.question(`베팅할 금액을 입력하세요. 소지금 : ${player.money}> `));
+        console.log(`Betting Money : ${this.bettingMoney}`);
+        return player.money - this.bettingMoney;
+    }
 
+    initObjects(){
+        this.deck = getNewDeck();
+        this.dealer = new Dealer();
+        this.player = new Player(this.userInfo,this.readlineSync);
     }
 
     startGame(){
         console.log('Start game!');
-        this.deck = getNewDeck();
-        this.dealer = new Dealer();
-        this.shuffleDeck();
+        this.initObjects();
+        this.deck = this.shuffleDeck(this.deck);
 
-        this.betMoney();
+        this.dealCards();
+
+        this.player.money = this.betMoney(this.player);
+
+        
 
 
 
-        this.printPlayerMoney();
+        // this.printPlayerMoney();
         
     }
 
