c0dff968012ab4b6919e618adb608a364312ee03
wangmin <0201.mj.lee@gmail.com>
2019-06-04 16:37:11 +0900

    feat : feat countSum method
    - feat replaceNumber method
    - refactor printHands method

--- blackjack.js
+++ blackjack.js
@@ -40,7 +40,45 @@ module.exports = class Blackjack{
         for(let value of player.hand){
             process.stdout.write(`${value.suit}  ${value.number} `);
         }
-        console.log('');
+        console.log(` sum : ${this.countSum(player.hand)}`);
+    }
+
+    replaceNumber(character) {
+        if (character === 'J' || character === 'Q' || character === 'K') {
+            return [10];
+        } else if (character == 'A') {
+            return [1, 11];
+        }
+        return [Number(character)];
+    }
+
+    countSum(hand) {
+        let sum = [0, 0];
+        let number;
+        for (let value of hand) {
+            if (sum[0] === 0) {
+                number = this.replaceNumber(value.number);
+                for (let i = 0; i < number.length; i++) {
+                    sum[i] += number[i];
+                }
+            } else if (sum[1] === 0) {
+                number = this.replaceNumber(value.number);
+                if (number.length > 1) {
+                    sum[1] = sum[0] + number[1];
+                    sum[0] += number[0];
+                } else {
+                    sum[0] += number[0];
+                }
+            } else {
+                sum[0] += number[0];
+                sum[1] += number[0];
+            }
+        }
+        if (sum[1] === 0) {
+            return [sum[0]];
+        } else {
+            return sum;
+        }
     }
 
     startGame(){
@@ -52,6 +90,8 @@ module.exports = class Blackjack{
         this.dealCards();
         this.printHands(this.dealer);
         this.printHands(this.player);
+        // console.log(this.countSum(this.dealer.hand));
+        // console.log(this.countSum(this.player.hand));
         
 
 
