dd771b8746e3bc273b12795e392b2e617e738598
wangmin <0201.mj.lee@gmail.com>
2019-06-06 14:19:50 +0900

    refactor : refactor shuffleDeck method

--- dealer.js
+++ dealer.js
@@ -1,9 +1,10 @@
 module.exports = class Dealer{
     constructor(){
-        this.hand;
+        this.id = 'Dealer';
+        this.hand = [];
     }
     shuffleDeck(deck){
-        const newDeck = Array.from(deck);
+        const newDeck = [...deck];
         for(let i = newDeck.length -1; i > 0 ; i--){
             const randomIndex = Math.floor(Math.random()*(i+1));
             const randomCard = newDeck[i];
