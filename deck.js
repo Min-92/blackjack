27868a6555282d73ceade2852d2826759ca43d45
wangmin <0201.mj.lee@gmail.com>
2019-06-07 12:16:01 +0900

    refactor : refactor deck.js
    - add Deck class method
    - add shuffleCardList method
    - add drawCard method
    - add dealCard method

--- deck.js
+++ deck.js
@@ -1,7 +1,5 @@
-const deck = [];
-
 const suitList = ['◇','♧','♤','♡'];
-const numberList = ['1','2','3','4','5','6','7','8','9','10','J','Q','K'];
+const numberList = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
 
 class card {
     constructor(suit,number){
@@ -10,12 +8,37 @@ class card {
     }
 }
 
-module.exports = getNewDeck = () => {
-    for(let i = 0; i < 4; i++){
-        for(let j = 0; j < 13; j++){
-            deck.push(new card(suitList[i],numberList[j]));
-        }
+module.exports = class Deck {
+    constructor(){
+        this.cardList = [];
     }
-    return deck;
-}
 
+    getNewDeck(){
+        const newDeck = [];
+        for(let i = 0; i < 4; i++){
+            for(let j = 0; j < 13; j++){
+                newDeck.push(new card(suitList[i],numberList[j]));
+            }
+        }
+        this.cardList = newDeck;
+    }
+
+    shuffleCardList(){
+        const newDeck = [...this.cardList];
+        for(let i = newDeck.length -1; i > 0 ; i--){
+            const randomIndex = Math.floor(Math.random()*(i+1));
+            const randomCard = newDeck[i];
+            newDeck[i] = newDeck[randomIndex];
+            newDeck[randomIndex] = randomCard;
+        }
+        this.cardList = newDeck;
+    }
+
+    drawCard(){
+        return this.cardList.pop();
+    }
+
+    dealCard(){
+        return drawCard();
+    }
+}
