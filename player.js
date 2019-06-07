a9cac271ab6de9315442180dc42eb0cb6d0184a0
wangmin <0201.mj.lee@gmail.com>
2019-06-07 12:39:17 +0900

    refactor : refactor player.js
    - add takeCard method

--- player.js
+++ player.js
@@ -1,16 +1,17 @@
-module.exports = class player{
-    constructor(userInfo,readlineSync){
+module.exports = class Player{
+    constructor(userInfo,readlineSync,deck){
         this.readlineSync = readlineSync;
 
         this.id = userInfo.id;
         this.money = userInfo.money;
         this.hand = [];
+        this.deck = deck;
     }
 
     betMoney(bettingMoney){
         bettingMoney = Number(this.readlineSync.question(`베팅할 금액을 입력하세요. 소지금 : ${this.money}> `));
         if(bettingMoney > this.money){
-            console.log(`베팅금액은 소지금보다 작아야합니다. 소지금 : ${this.money}> `);
+            console.log(`베팅금액은 소지금보다 적어야합니다.`);
             this.betMoney(bettingMoney);
         }else{
             this.money -= bettingMoney;
@@ -18,5 +19,8 @@ module.exports = class player{
         }
     }
 
+    takeCard(){
+        this.hand.push(this.deck.dealCard());
+    }
 
 }
