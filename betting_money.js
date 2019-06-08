905550614245af0fe19c2ca577c92ad5d47934bc
wangmin <0201.mj.lee@gmail.com>
2019-06-08 17:32:31 +0900

    feat : feat betting money class
    - add this.amount
    - add rewardMoney method
    - add takeMoney method

--- betting_money.js
+++ betting_money.js
@@ -1,4 +1,17 @@
 module.exports = class BettingMoney{
     constructor(){
+        this.amount = 0;
     }
+
+    rewardMoney(){
+        const reward = this.amount*2;
+        this.amount = 0;
+        return reward;
+    }
+
+    takeMoney(){
+        this.amount = 0; 
+    }
+
+
 }
