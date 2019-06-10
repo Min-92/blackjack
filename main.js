38d778db64c01f0bfa1286751e9a0af39a297867
wangmin <0201.mj.lee@gmail.com>
2019-06-10 22:59:45 +0900

    feat : reqire draw.js
    - delete restart

--- main.js
+++ main.js
@@ -7,36 +7,38 @@ const Player = require('./player.js');
 const Deck = require('./deck.js');
 const Rule = require('./rule.js');
 const BettingMoney = require('./betting_money.js');
+const Draw = require('./draw.js');
 
 const socket = net.connect(
     { host: 'localhost', port: 3000 }
     , () => {
-        console.log(`Welcome to Wang's Blackjack`);
+        // console.log(`Welcome to Wang's Blackjack`);
         main();
     }
 );
 
-const userManager = new UserManager(socket,readlineSync);
+const draw = new Draw();
+
+const userManager = new UserManager(socket, readlineSync, draw);
 
 const initObjects = (userInfo) => {
     const bettingMoney = new BettingMoney();
     const deck = new Deck();
     deck.getNewDeck();
     const rule = new Rule()
-    const player = new Player(userInfo, readlineSync, deck, rule);
-    const dealer = new Dealer(deck, player, rule);
+    const player = new Player(userInfo, readlineSync, deck, rule, draw);
+    const dealer = new Dealer(deck, player, rule, draw);
     const argumentsObject = {
-        player, dealer, bettingMoney, deck, socket, readlineSync, rule
+        player, dealer, bettingMoney, deck, socket, readlineSync, rule, draw
     }
     return new Blackjack(argumentsObject);
 }
 
-const main = async() => {
+const main = async () => {
+    draw.setScreen();
     const userInfo = await userManager.getUserInfo();
+    draw.removePrompt();
+    draw.removeBox();
     const blackjack = initObjects(userInfo);
-    console.log('Start game!');
-    let restart = true;
-    while(restart){
-        restart = blackjack.playGame();
-    }
+    blackjack.startGame();
 }
